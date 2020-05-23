6ec7495ab2a35cf53e6066e7387f4a924cca6511
divyashri-2020 <65487937+divyashri-2020@users.noreply.github.com>
2020-05-23 18:39:42 +0530

    Add files via upload

---
+++ Divyashri.B_4AL17EC031.py
@@ -0,0 +1,10 @@
+for i in range(3):
+x=input("username")
+y=input("password")
+if x=="Micheal" and y=="e3$WT89x":
+print("logged in succesfully")
+break
+elif:
+print("username/password is invalid")
+else i==2:
+print("account locked")
